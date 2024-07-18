840007f22c5d7208699d33ab35270a1745d29c21
Wauan Matheus <151370704+wauanmatheus@users.noreply.github.com>
2024-07-18 10:11:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,6 @@ imagem2.addEventListener("mouseover", function () {
   imagem2.src = "img/lampada-on.jpg";
 }); 
 
-
 imagem2.addEventListener("mouseout", function () {
     imagem2.src = "img/lampada.jpg";
 });
